7c43719b1527860b7db56cc683e0b66e9c2e12aa
Artem <kulberg.artem@yandex.ru>
2022-09-07 17:51:22 +0300

    Create comment.php

---
+++ email-temp/comment.php
@@ -0,0 +1,150 @@
+<?php
+
+$a_comment = array(
+    'https://hyper-fin-market.ru/img/email-img/sm-vk-2.png',
+    'https://hyper-fin-market.ru/img/email-img/sm-tg-2.png',
+    'https://hyper-fin-market.ru/img/email-img/sm-youtube-2.png',
+    'https://hyper-fin-market.ru/img/email-img/as-2.png',
+    'https://hyper-fin-market.ru/img/email-img/gp-2.png',
+);
+
+$e_text_comment = '<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="x-apple-disable-message-reformatting">
+    <title></title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,600" rel="stylesheet" type="text/css">
+    <!-- Web Font / @font-face : BEGIN -->
+    <!--[if mso]>
+    <style>
+        * {
+            font-family: \'Roboto\', sans-serif !important;
+        }
+    </style>
+    <![endif]-->
+
+    <!--[if !mso]>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,600" rel="stylesheet" type="text/css">
+    <![endif]-->
+
+    <!-- Web Font / @font-face : END -->
+
+    <!-- CSS Reset : BEGIN -->
+
+    <style>
+        /* What it does: Remove spaces around the email design added by some email clients. */
+        /* Beware: It can remove the padding / margin and add a background color to the compose a reply window. */
+        html,
+        body {
+            margin: 0 auto !important;
+            padding: 0 !important;
+            height: 100% !important;
+            width: 100% !important;
+            font-family: \'Roboto\', sans-serif !important;
+            font-size: 14px;
+            margin-bottom: 10px;
+            line-height: 24px;
+            color: black;
+            font-weight: 400;
+        }
+        * {
+            -ms-text-size-adjust: 100%;
+            -webkit-text-size-adjust: 100%;
+            margin: 0;
+            padding: 0;
+        }
+        table,
+        td {
+            mso-table-lspace: 0pt !important;
+            mso-table-rspace: 0pt !important;
+        }
+        table {
+            border-spacing: 0 !important;
+            border-collapse: collapse !important;
+            table-layout: fixed !important;
+            margin: 0 auto !important;
+        }
+        table table table {
+            table-layout: auto;
+        }
+        a {
+            text-decoration: none;
+        }
+        img {
+            -ms-interpolation-mode:bicubic;
+        }
+    </style>
+</head>
+
+<body width="100%" style="margin: 0; padding: 0 !important; mso-line-height-rule: exactly; background-color: #f5f6fa;">
+<center style="width: 100%; background-color: #f5f6fa;">
+    <table width="100%" border="0" cellpadding="0" cellspacing="0" bgcolor="#f5f6fa">
+        <tr>
+            <td style="padding: 25px 0;">
+                <table style="width:100%;max-width:620px;margin:0 auto;">
+                    <tbody>
+                    <tr>
+                        <td style="text-align: center; padding-bottom:25px">
+                            <p style="font-family: Verdana; font-size: 22px; font-weight: 700; color: #6DA3BD; margin-bottom: 0">SMART-LAB</p>
+                            <p style="font-size: 14px; color: #000000; padding: 6px">Крупнейшее в России сообщество инвесторов и трейдеров.</p>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+                <table style="width:100%;max-width:620px;margin:0 auto;background-color:#ffffff;">
+                    <tbody>
+                        <tr>
+                            <td style="padding: 30px 30px 0 30px;">
+                                <h5 style="margin-bottom: 15px; color: #000000; font-size: 20px; font-weight: 400; line-height: 28px;">Новый комментарий</h5>
+                                <p style="margin-bottom: 15px; color: #000000; font-size: 16px;"><a style=" color: #0077B5;text-decoration:underline;" href="#" target="_blank">mushrooms_invest</a>:</p>
+                                <p style="margin-bottom: 20px; color: #000000; font-size: 14px; padding-left: 20px; border-left: 1px solid #9ea8bb">Новый исторический рекорд суточного объема поставок российского газа в Китай по газопроводу "Сила Сибири" Поставки осуществляются в рамках долгосрочного контракта между «Газпромом» и китайской CNPC. 17 июля зафиксирован новый исторический рекорд суточного объема «Газпром» зафиксировал новый рекорд суточного объема поставок газа в КНР по «Силе Сибири» — ТАСС (tass.ru) Авто-репост.</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="padding: 0 30px; text-align: right; ">
+                                <ul style="margin: 10px -4px 20px;padding: 0;">
+                                    <li style="display: inline-block; list-style: none; padding: 4px;">
+                                        <a href="#" style="background-color:#f5f6fa;border-radius:4px;color:#0077B5;display:inline-block;font-size:13px;font-weight:500;line-height:34px;text-align:center;text-decoration:none;text-transform: uppercase; width: 100px">на форум</a>
+                                    </li>
+                                    <li style="display: inline-block; list-style: none; padding: 4px;">
+                                        <a href="#" style="background-color:#0077B5;border-radius:4px;color:#ffffff;display:inline-block;font-size:13px;font-weight:500;line-height:34px;text-align:center;text-decoration:none;text-transform: uppercase; width: 100px">Ответить</a>
+                                    </li>
+
+                                </ul>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="text-align:center;padding: 0 30px 30px">
+                                <p style="margin: 0; font-size: 13px; line-height: 22px; color:#9ea8bb;">Отписаться от уведомлений в форуме «Газпром» <a style="color: #6576ff; text-decoration:underline;" href="#" target="_blank">здесь</a>.</p>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table style="width:100%;max-width:620px;margin:0 auto;">
+                    <tbody>
+                    <tr>
+                        <td style="text-align: center; padding:10px 20px 0;">
+                            <ul style="margin: 10px -4px 0;padding: 0;">
+                                <li style="display: inline-block; list-style: none; padding: 4px; height: 30px; width:30px;"><a style="display: inline-block; height: 30px; width:30px;border-radius: 50%; background-color: #ffffff" href="https://vk.com/smartlabru" target="_blank"><img style="width: 30px" src="cid:sm-vk-2.png" alt="brand"></a></li>
+                                <li style="display: inline-block; list-style: none; padding: 4px; height: 30px; width:30px;"><a style="display: inline-block; height: 30px; width:30px;border-radius: 50%; background-color: #ffffff" href="https://t.me/smartlabnews" target="_blank"><img style="width: 30px" src="cid:sm-tg-2.png" alt="brand"></a></li>
+                                <li style="display: inline-block; list-style: none; padding: 4px; height: 30px; width:30px;"><a style="display: inline-block; height: 30px; width:30px;border-radius: 50%; background-color: #ffffff" href="https://www.youtube.com/user/Chiefanalyst" target="_blank"><img style="width: 30px" src="cid:sm-youtube-2.png" alt="brand"></a></li>
+                            </ul>
+                            <p style="padding-top: 15px; font-size: 14px; margin-bottom: 1rem;">Установите приложение смартлаба:</p>
+                            <ul style="margin: 10px -4px 0;padding: 0;">
+                                <li style="display: inline-block; list-style: none; padding: 4px;"><a style="display: inline-block;" href="https://play.google.com/store/apps/details?id=ru.smartlab" target="_blank"><img style="width: 90px" src="cid:gp-2.png" alt="brand"></a></li>
+                                <li style="display: inline-block; list-style: none; padding: 4px;"><a style="display: inline-block;" href="https://apps.apple.com/ru/app/smartlab/id1602002316" target="_blank"><img style="width: 90px" src="cid:as-2.png" alt="brand"></a></li>
+                            </ul>
+                            <p style="padding-top: 15px; font-size: 12px; color: #000000;">Это письмо было отправлено Вам, так как вы зарегистрированы на <a style="color: #6576ff; text-decoration:underline;" href="https://smart-lab-prem-posts.ru/" target="_blank">smart-lab-prem-posts.ru</a>.<br> Чтобы отписаться от рассылки нажмите <a style="color: #6576ff; text-decoration:underline;" href="#" target="_blank">здесь</a>.</p>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+    </table>
+</center>
+</body>
+</html>';
