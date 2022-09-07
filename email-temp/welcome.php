529552d37fa06ae9cac7cfb9178484d84ff8b46b
Artem <kulberg.artem@yandex.ru>
2022-09-07 17:51:37 +0300

    Create welcome.php

---
+++ email-temp/welcome.php
@@ -0,0 +1,145 @@
+<?php
+
+$a_welcome = array(
+    'https://hyper-fin-market.ru/img/email-img/sm-vk-2.png',
+    'https://hyper-fin-market.ru/img/email-img/sm-tg-2.png',
+    'https://hyper-fin-market.ru/img/email-img/sm-youtube-2.png',
+    'https://hyper-fin-market.ru/img/email-img/as-2.png',
+    'https://hyper-fin-market.ru/img/email-img/gp-2.png',
+);
+
+$e_text_welcome = '<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="x-apple-disable-message-reformatting">
+    <title></title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,600" rel="stylesheet" type="text/css">
+    <!-- Web Font / @font-face : BEGIN -->
+    <!--[if mso]>
+    <style>
+        * {
+            font-family: \'Roboto\', sans-serif !important;
+        }
+    </style>
+    <![endif]-->
+
+    <!--[if !mso]>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,600" rel="stylesheet" type="text/css">
+    <![endif]-->
+
+    <!-- Web Font / @font-face : END -->
+
+    <!-- CSS Reset : BEGIN -->
+
+    <style>
+        /* What it does: Remove spaces around the email design added by some email clients. */
+        /* Beware: It can remove the padding / margin and add a background color to the compose a reply window. */
+        html,
+        body {
+            margin: 0 auto !important;
+            padding: 0 !important;
+            height: 100% !important;
+            width: 100% !important;
+            font-family: \'Roboto\', sans-serif !important;
+            font-size: 14px;
+            margin-bottom: 10px;
+            line-height: 24px;
+            color:black;
+            font-weight: 400;
+        }
+        * {
+            -ms-text-size-adjust: 100%;
+            -webkit-text-size-adjust: 100%;
+            margin: 0;
+            padding: 0;
+        }
+        table,
+        td {
+            mso-table-lspace: 0pt !important;
+            mso-table-rspace: 0pt !important;
+        }
+        table {
+            border-spacing: 0 !important;
+            border-collapse: collapse !important;
+            table-layout: fixed !important;
+            margin: 0 auto !important;
+        }
+        table table table {
+            table-layout: auto;
+        }
+        a {
+            text-decoration: none;
+        }
+        img {
+            -ms-interpolation-mode:bicubic;
+        }
+    </style>
+</head>
+
+<body width="100%" style="margin: 0; padding: 0 !important; mso-line-height-rule: exactly; background-color: #f5f6fa;">
+<center style="width: 100%; background-color: #f5f6fa;">
+    <table width="100%" border="0" cellpadding="0" cellspacing="0" bgcolor="#f5f6fa">
+        <tr>
+            <td style="padding: 25px 0;">
+                <table style="width:100%;max-width:620px;margin:0 auto;">
+                    <tbody>
+                    <tr>
+                        <td style="text-align: center; padding-bottom:25px">
+                            <p style="font-family: Verdana; font-size: 22px; font-weight: 700; color: #6DA3BD; margin-bottom: 0">SMART-LAB</p>
+                            <p style="font-size: 14px; color: #000000; padding: 6px">Крупнейшее в России сообщество инвесторов и трейдеров.</p>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+                <table style="width:100%;max-width:620px;margin:0 auto;background-color:#ffffff;">
+                    <tbody>
+                    <tr>
+                        <td style="padding: 30px 30px 20px">
+                            <p style="margin-bottom: 10px; color: #000000;">Приветствуем Вас!</p>
+                            <p style="margin-bottom: 10px; color: #000000;">Вы успешно прошли регистрацию на сайте <b>Smart-Lab</b>.</p>
+                            <p style="margin-bottom: 10px; line-height: normal; color: #000000;">
+                                Через 24 часа вы сможете написать в <a style="color: #6576ff; text-decoration:underline;" href="https://smart-lab-prem-posts.ru/topic/add/?utm_medium=email&utm_content=notify" target="_blank">cвой блог</a> и оставлять комментарии.<br>
+                                Чтобы набрать <a style="color: #6576ff; text-decoration:underline;" href="https://smart-lab-prem-posts.ru/company/smartlabru/blog/398497.php?utm_medium=email&utm_content=notify" target="_blank">рейтинг</a>, напишите пару интересных постов, например о том, что привело вас на биржу и на Smart-Lab в частности.
+                            </p>
+
+                            <p style="margin-bottom: 10px; color: #000000;">Читать все посты подряд на смартлабе вы можете в <a style="color: #6576ff; text-decoration:underline;" href="https://smart-lab-prem-posts.ru/allblog/?utm_medium=email&utm_content=notify" target="_blank">ленте</a>.</p>
+                            <p style="margin-bottom: 10px; color: #000000;">Посмотрите <a style="color: #6576ff; text-decoration:underline;" href="https://smart-lab-prem-posts.ru/people/?utm_medium=email&utm_content=notify" target="_blank">самых популярных авторов</a> и подпишитесь, чтобы читать их в своей <a style="color: #6576ff; text-decoration:underline;" href="https://smart-lab-prem-posts.ru/friends/kulbart?utm_medium=email&utm_content=notify" target="_blank">ленте друзей</a>.</p>
+                            <p style="margin-bottom: 10px; line-height: normal; color: #000000;">
+                                Загляните на наш <a style="color: #6576ff; text-decoration:underline;" href="https://smart-lab-prem-posts.ru/trading/?utm_medium=email&utm_content=notify" target="_blank">форум</a>, чтобы принять участие в обсуждении конкретных тем.
+                                <br>
+                                Чтобы сделать общение более живым, мы рекомендуем загружать в аватар свое настоящее фото и указывать свое имя. Это можно сделать в <a style="color: #6576ff; text-decoration:underline;" href="https://smart-lab-prem-posts.ru/settings/?utm_medium=email&utm_content=notify" target="_blank">настройках</a>.
+                            </p>
+                            <p style="margin-bottom: 15px; color: #000000;">Если у вас есть вопросы, зайдите в раздел <a style="color: #6576ff; text-decoration:underline;" href="https://smart-lab-prem-posts.ru/page/help/?utm_medium=email&utm_content=notify">Помощь</a> или вы можете задать их <a style="color: #6576ff; text-decoration:underline;" href="https://smart-lab-prem-posts.ru/trading/vopros/?utm_medium=email&utm_content=notify" target="_blank">тут</a>.</p>
+                            <p style="margin-bottom: 10px; color: #000000;">Удачных инвестиций!</p>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+                <table style="width:100%;max-width:620px;margin:0 auto;">
+                    <tbody>
+                    <tr>
+                        <td style="text-align: center; padding:10px 20px 0;">
+                            <ul style="margin: 10px -4px 0;padding: 0;">
+                                <li style="display: inline-block; list-style: none; padding: 4px; height: 30px; width:30px;"><a style="display: inline-block; height: 30px; width:30px;border-radius: 50%; background-color: #ffffff" href="https://vk.com/smartlabru" target="_blank"><img style="width: 30px" src="cid:sm-vk-2.png" alt="brand"></a></li>
+                                <li style="display: inline-block; list-style: none; padding: 4px; height: 30px; width:30px;"><a style="display: inline-block; height: 30px; width:30px;border-radius: 50%; background-color: #ffffff" href="https://t.me/smartlabnews" target="_blank"><img style="width: 30px" src="cid:sm-tg-2.png" alt="brand"></a></li>
+                                <li style="display: inline-block; list-style: none; padding: 4px; height: 30px; width:30px;"><a style="display: inline-block; height: 30px; width:30px;border-radius: 50%; background-color: #ffffff" href="https://www.youtube.com/user/Chiefanalyst" target="_blank"><img style="width: 30px" src="cid:sm-youtube-2.png" alt="brand"></a></li>
+                            </ul>
+                            <p style="padding-top: 15px; font-size: 14px; margin-bottom: 1rem;">Установите приложение смартлаба:</p>
+                            <ul style="margin: 10px -4px 0;padding: 0;">
+                                <li style="display: inline-block; list-style: none; padding: 4px;"><a style="display: inline-block;" href="https://play.google.com/store/apps/details?id=ru.smartlab" target="_blank"><img style="width: 90px" src="cid:gp-2.png" alt="brand"></a></li>
+                                <li style="display: inline-block; list-style: none; padding: 4px;"><a style="display: inline-block;" href="https://apps.apple.com/ru/app/smartlab/id1602002316" target="_blank"><img style="width: 90px" src="cid:as-2.png" alt="brand"></a></li>
+                            </ul>
+                            <p style="padding-top: 15px; font-size: 12px;">Это письмо было отправлено Вам, так как вы зарегистрированы на <a style="color: #6576ff; text-decoration:underline;" href="https://smart-lab-prem-posts.ru/" target="_blank">smart-lab-prem-posts.ru</a>.<br> Чтобы отписаться от рассылки нажмите <a style="color: #6576ff; text-decoration:underline;" href="#" target="_blank">здесь</a>.</p>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+    </table>
+</center>
+</body>
+</html>';
